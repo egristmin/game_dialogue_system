3696bc1d9d7bd06f1f8d9658ea430520a1ef2435
egristmin <egristmin@gmail.com>
2024-12-26 23:03:10 +0300

    library

--- lib/game_dialogue_system.dart
+++ lib/game_dialogue_system.dart
@@ -1,5 +1,6 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library;
+
+export 'answer.dart';
+export 'dialogue.dart';
+export 'question.dart';
+export 'typedef.dart';
