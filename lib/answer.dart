9de914ea334935fb320ce264ba03e85d4166203a
egristmin <egristmin@gmail.com>
2024-12-29 16:27:27 +0300

    answer json serialization

--- lib/answer.dart
+++ lib/answer.dart
@@ -12,14 +12,30 @@ class Answer {
     this.finishDialogue = false,
   });
 
+  Answer.fromJson(Map<String, dynamic> json, Dialogue reference)
+      : dialogue = reference,
+        id = json['id'] as int,
+        stateCondition = json['stateCondition'] as String?,
+        content = json['content'] as String,
+        event = json['event'] as String?,
+        finishDialogue = (json['finishDialogue'] as bool?) ?? false;
+
+  final Dialogue dialogue;
   final AnswerId id;
   final String? stateCondition;
   final String content;
-  final Dialogue dialogue;
   final String? event;
   final bool finishDialogue;
+
   bool get hasStateCondition => stateCondition != null;
 
-
   void choice() => dialogue.onAnswerChoice(this);
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        if (stateCondition != null) 'stateCondition': stateCondition,
+        'content': content,
+        if (event != null) 'event': event,
+        'finishDialogue': finishDialogue,
+      };
 }
