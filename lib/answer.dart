f794ccc3576e8934447b5c80b9074a619774c806
egristmin <egristmin@gmail.com>
2024-12-29 16:47:25 +0300

    answer dialogue reference removed

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,10 +1,7 @@
-import 'package:game_dialogue_system/dialogue.dart';
-
 import 'typedef.dart';
 
 class Answer {
   const Answer({
-    required this.dialogue,
     required this.id,
     required this.content,
     this.stateCondition,
@@ -12,15 +9,13 @@ class Answer {
     this.finishDialogue = false,
   });
 
-  Answer.fromJson(Map<String, dynamic> json, Dialogue reference)
-      : dialogue = reference,
-        id = json['id'] as int,
+  Answer.fromJson(Map<String, dynamic> json)
+      : id = json['id'] as int,
         stateCondition = json['stateCondition'] as String?,
         content = json['content'] as String,
         event = json['event'] as String?,
         finishDialogue = (json['finishDialogue'] as bool?) ?? false;
 
-  final Dialogue dialogue;
   final AnswerId id;
   final String? stateCondition;
   final String content;
@@ -28,8 +23,6 @@ class Answer {
   final bool finishDialogue;
 
   bool get hasStateCondition => stateCondition != null;
-
-  void choice() => dialogue.onAnswerChoice(this);
 
   Map<String, dynamic> toJson() => {
         'id': id,
