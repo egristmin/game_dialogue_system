79b8a47dbc21ece1b4ee3779a1cd16362206cf67
egristmin <egristmin@gmail.com>
2024-12-29 16:51:35 +0300

    dialogue config from/to json

--- lib/dialogue_config.dart
+++ lib/dialogue_config.dart
@@ -5,4 +5,17 @@ class DialogueConfig {
   final List<Question> questions;
 
   const DialogueConfig({this.questions = const [], this.answers = const []});
+
+  DialogueConfig.fromJson(Map<String, dynamic> json)
+      : answers = ((json['answers'] ?? []) as List<Map<String, dynamic>>)
+            .map((Answer.fromJson))
+            .toList(),
+        questions = ((json['questions'] ?? []) as List<Map<String, dynamic>>)
+            .map((Question.fromJson))
+            .toList();
+
+  Map<String, dynamic> toJson() => {
+        'answers': answers.map((answer) => answer.toJson()).toList(),
+        'questions': questions.map((question) => question.toJson()).toList(),
+      };
 }
